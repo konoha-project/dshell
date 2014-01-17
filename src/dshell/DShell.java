29711784a7a1604e5d865518f4e4ec27d91b9647
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-01-17 17:08:46 +0900

    fix prompt update to libzen 53f8e93dacf08c4e2833b9e6759edfecb977236d

--- src/dshell/DShell.java
+++ src/dshell/DShell.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.TreeSet;
 
+import jnr.ffi.Struct.int16_t;
+
 import dshell.codegen.javascript.ModifiedJavaScriptSourceGenerator;
 import dshell.lang.DShellGrammar;
 import zen.deps.LibNative;
@@ -15,25 +17,25 @@ import zen.parser.ZenParserConst;
 
 public class DShell {
 	public static void main(String[] args) {
-		DShellConsole Console = new DShellConsole();
+		DShellConsole console = new DShellConsole();
 		boolean interactiveMode = true;
-		ZenGenerator Generator = new ModifiedJavaScriptSourceGenerator(); //TODO: using JavaByteCodeGen
-		LibNative.ImportGrammar(Generator.RootNameSpace, ZenGrammar.class.getName());
-		LibNative.ImportGrammar(Generator.RootNameSpace, DShellGrammar.class.getName());
+		ZenGenerator generator = new ModifiedJavaScriptSourceGenerator(); //TODO: using JavaByteCodeGen
+		LibNative.ImportGrammar(generator.RootNameSpace, ZenGrammar.class.getName());
+		LibNative.ImportGrammar(generator.RootNameSpace, DShellGrammar.class.getName());
 		if(interactiveMode) {
 			System.out.println(ZenParserConst.ProgName + ZenParserConst.Version + " (" + ZenParserConst.CodeName + ") on " + LibZen.GetPlatform());
 			System.out.println(ZenParserConst.Copyright);	//FIXME
-			Generator.Logger.ShowReportedErrors();
+			generator.Logger.ShowReportedErrors();
 			int linenum = 1;
-			String Line = null;
-			while ((Line = Console.ReadLine(">>> ", "    ")) != null) {
+			String line = null;
+			while ((line = console.readLine()) != null) {
 				try {
-					Object EvaledValue = Generator.RootNameSpace.Eval(Line, linenum, interactiveMode);
-					Generator.Logger.ShowReportedErrors();
-					if (EvaledValue != null) {
-						System.out.println(" (" + ZenSystem.GuessType(EvaledValue) + ":");
-						System.out.println(LibNative.GetClassName(EvaledValue)+ ") ");
-						System.out.println(LibZen.Stringify(EvaledValue));
+					Object evaledValue = generator.RootNameSpace.Eval(line, linenum, interactiveMode);
+					generator.Logger.ShowReportedErrors();
+					if (evaledValue != null) {
+						System.out.println(" (" + ZenSystem.GuessType(evaledValue) + ":");
+						System.out.println(LibNative.GetClassName(evaledValue)+ ") ");
+						System.out.println(LibZen.Stringify(evaledValue));
 					}
 					linenum++;
 				}
@@ -48,56 +50,60 @@ public class DShell {
 }
 
 class DShellConsole {
-	private jline.ConsoleReader ConsoleReader = null;
+	private jline.ConsoleReader consoleReader = null;
+	private String userName = System.getProperty("user.name");
 	
 	public DShellConsole() {
 		try {
-			this.ConsoleReader = new jline.ConsoleReader();
-			jline.SimpleCompletor CommandCompletor = new jline.SimpleCompletor("dummy");
-			CommandCompletor.setCandidates(GetCommandListFromPath());
-			jline.Completor[] Completors = new jline.Completor[2];
-			Completors[0] = CommandCompletor;
-			Completors[1] = new jline.FileNameCompletor();
-			this.ConsoleReader.addCompletor(new jline.ArgumentCompletor(Completors));
+			this.consoleReader = new jline.ConsoleReader();
+			jline.SimpleCompletor commandCompletor = new jline.SimpleCompletor("dummy");
+			commandCompletor.setCandidates(getCommandListFromPath());
+			jline.Completor[] completors = new jline.Completor[2];
+			completors[0] = commandCompletor;
+			completors[1] = new jline.FileNameCompletor();
+			this.consoleReader.addCompletor(new jline.ArgumentCompletor(completors));
 		}
 		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public final String ReadLine(String Prompt, String Prompt2) {
-		String Line;
+	public final String readLine() {
+		String[] prompts = this.getPrompts();
+		String prompt = prompts[0];
+		String prompt2 = prompts[1];
+		String line;
 		try {
-			Line = this.ConsoleReader.readLine(Prompt);
+			line = this.consoleReader.readLine(prompt);
 		}
 		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		if(Line == null) {
+		if(line == null) {
 			System.exit(0);
 		}
-		if(Prompt2 != null) {
+		if(prompt2 != null) {
 			int level = 0;
-			while((level = LibZen.CheckBraceLevel(Line)) > 0) {
+			while((level = LibZen.CheckBraceLevel(line)) > 0) {
 				String Line2;
 				try {
-					Line2 = this.ConsoleReader.readLine(Prompt2);
+					Line2 = this.consoleReader.readLine(prompt2);
 				}
 				catch (IOException e) {
 					throw new RuntimeException(e);
 				}
-				Line += "\n" + Line2;
+				line += "\n" + Line2;
 			}
 			if(level < 0) {
-				Line = "";
-				LibNative.println(" .. canceled");
+				line = "";
+				System.out.println(" .. canceled");
 			}
 		}
-		this.ConsoleReader.getHistory().addToHistory(Line);
-		return Line;
+		this.consoleReader.getHistory().addToHistory(line);
+		return line;
 	}
 
-	private final TreeSet<String> GetCommandListFromPath() {
+	private final TreeSet<String> getCommandListFromPath() {
 		TreeSet<String> commandSet = new TreeSet<String>();
 		String[] paths = System.getenv("PATH").split(":");
 		for(int i = 0; i < paths.length; i++) {
@@ -112,5 +118,24 @@ class DShellConsole {
 			}
 		}
 		return commandSet;
+	}
+
+	private String[] getPrompts() {
+		String homeDir = System.getenv("HOME");
+		String[] prompts = new String[2];
+		String currentDir = System.getProperty("user.dir");
+		if(currentDir.startsWith(homeDir)) {
+			int index = homeDir.length();
+			currentDir = "~" + currentDir.substring(index);
+		}
+		String prompt =  "[" + this.userName + "]:" + currentDir + "> ";
+		StringBuilder promptBuilder = new StringBuilder();
+		int size = prompt.length();
+		for(int i = 0; i < size; i++) {
+			promptBuilder.append(" ");
+		}
+		prompts[0] = prompt;
+		prompts[1] = promptBuilder.toString();
+		return prompts;
 	}
 }
