1c0a337db43141be2a84fdec867a36d108555465
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-02-21 22:01:04 +0900

    insert assert function to test case

--- test/test_all.sh
+++ test/test_all.sh
@@ -7,6 +7,7 @@ fi
 
 for test_dir in `ls ${TEST_DIR}/tests`; do
 	for test_file in `ls ${TEST_DIR}/tests/$test_dir`; do
+		echo "====== Test: ${test_file} ======"
 		dshell $REC $TEST_DIR/tests/$test_dir/$test_file
 	done
 done
