ea724094606f8e397750052a1fd9a24349378987
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-04-05 00:42:11 +0900

    version v0.2.2

--- src/dshell/DShell.java
+++ src/dshell/DShell.java
@@ -21,7 +21,7 @@ public class DShell {
 	public final static int majorVersion = 0;
 	public final static int minerVersion = 1;
 	public final static int patchLevel   = 0;
-	public final static String version = "0.2";
+	public final static String version = "0.2.2";
 	public final static String copyright = "Copyright (c) 2013-2014, Konoha project authors";
 	public final static String license = "BSD-Style Open Source";
 	public final static String shellInfo = progName + ", version " + version + " (" + LibZen._GetPlatform() + ") powered by LibZen";
