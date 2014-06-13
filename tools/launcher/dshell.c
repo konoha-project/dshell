781cab5ca83363c2097c5fc972fda3b085a2fc98
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-06-14 03:34:07 +0900

    reduce boot time by adding some option

--- tools/launcher/dshell.c
+++ tools/launcher/dshell.c
@@ -9,18 +9,21 @@
 #define XSTR(s) STR(s)
 #define STR(s) #s
 #define DSHELL_JAR_FILE XSTR(JAR_PATH) "/dshell.jar"
+#define BOOT_CLASSPATH "-Xbootclasspath/a:" DSHELL_JAR_FILE
 
 int main(int argc, char* argv[])
 {
-	int i, newargc = argc + 3;
+	int i, newargc = argc + 5;
 	char *newargv[newargc];
 	newargv[0] = "java";
-	newargv[1] = "-jar";
-	newargv[2] = DSHELL_JAR_FILE;
+	newargv[1] = BOOT_CLASSPATH;
+	newargv[2] = "-XX:+TieredCompilation";
+	newargv[3] = "-XX:TieredStopAtLevel=1";
+	newargv[4] = "dshell.internal.main.DShell";
 	for(i = 1; i < argc; i++) {
-		newargv[i + 2] = argv[i];
+		newargv[i + 4] = argv[i];
 	}
-	newargv[argc + 2] = NULL;
+	newargv[argc + 4] = NULL;
 	execvp(newargv[0], newargv);
 	perror("launching dshell failed\n");
 	return EXIT_FAILURE;
